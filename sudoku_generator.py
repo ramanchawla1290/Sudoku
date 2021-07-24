b42f428e43bfad6fa4f850102a4ad369044ddc31
Raman Chawla <ramanchawla1290@gmail.com>
2021-07-24 17:37:09 +0530

    Updated sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,1 +1,8 @@
+"""
+SUDOKU GENERATOR
 
+Creating a 9x9 Sudoku Board as per the following rules:
+1. In all the Nine 3×3 sub matrices, the elements should be 1-9, without repetition.
+2. In all the rows, the elements should be between 1-9 , without repetition.
+3. In all the columns, the elements should be between 1-9 , without repetition.
+"""
