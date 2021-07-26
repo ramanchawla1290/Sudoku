298a16fa318a9ae477c2e444e0a876e680660a28
Raman Chawla <ramanchawla1290@gmail.com>
2021-07-26 15:02:46 +0530

    Updated sudoku_generator.py : Checkpoint - Solved Sudoku Grids

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -11,17 +11,17 @@ from random import shuffle
 
 
 class Sudoku:
-    """Class to implement Sudoku board : generation and checking validity"""
+    """Class to implement Sudoku board generation and validity check"""
 
     def __init__(self):
         """Constructor"""
-        self.grid = self.__get_new_grid()
+        self.grid = Sudoku.__get_new_grid()
 
     @staticmethod
     def __get_new_grid():
-        """Creates and Returns new Sudoku Grid (9x9)"""
+        """Creates and Returns new Sudoku 9x9 Grid with populated diagonal blocks"""
 
-        # Creating 3x3 blocks filled with 0's
+        # Creating 9 blocks of size 3x3, filled with 0's
         blocks = [[[0] * 9 for i in range(3)] for j in range(3)]
 
         # Filling Diagaonal blocks with numbers 1-9 in random order
@@ -41,47 +41,99 @@ class Sudoku:
 
         return grid
 
-    def generate_new_grid(self):
-        """Assigns a new SUDOKU 9x9 grid to the Class instance"""
-        self.grid = self.__get_new_grid()
+    def __is_valid_grid(self):
+        """Validates the grid for size and values, Raises ValueError if invalid"""
+        if not isinstance(self.grid, list):
+            raise TypeError("Invalid type "
+                            + f"'{type(self.grid).__name__}' for Sudoku grid")
+
+        if len(self.grid) != 9:  # Checking if grid doesn't have 9 rows
+            raise ValueError("Invalid grid size! "
+                             + f"{len(self.grid)} rows in grid")
+
+        for i in range(9):  # Checking if any row doesn't have 9 numbers
+            if len(self.grid[i]) != 9:
+                raise ValueError("Invalid grid size! Only"
+                                 + f"{len(self.grid[i])} numbers in row {i}")
+
+        # PENDING : Checking of values
+
+    def __move_possible(self, row, col, num):
+        """Returns True if move is valid, False otherwise"""
+        # Checking possibility along row and column
+        for i in range(9):
+            if self.grid[row][i] == num or self.grid[i][col] == num:
+                return False
+
+        # Location of upper-left corner of the 3x3 block
+        x_0 = (row // 3) * 3
+        y_0 = (col // 3) * 3
+
+        # Checking possibility in the block
+        for i in range(3):
+            for j in range(3):
+                if self.grid[x_0 + i][y_0 + j] == num:
+                    return False
+        return True
+
+    def __solve(self):
+        """Replaces 0's in the Sudoku grid with valid numbers"""
+        for i in range(9):
+            for j in range(9):
+                if self.grid[i][j] == 0:
+                    for num in range(1, 10):
+                        if self.__move_possible(i, j, num):
+                            self.grid[i][j] = num
+                            if self.__solve():
+                                return True
+                            self.grid[i][j] = 0
+                    return
+
+        # When the grid is full
+        return True
+
+    def solve(self):
+        """Validates the grid, Returns the solved grid"""
+        try:
+            self.__is_valid_grid()
+        except ValueError as ex:
+            print(str(ex))
+        except TypeError as ex:
+            print(str(ex))
+        else:
+            print()
+            print("Sudoku Grid:")
+            self.print_grid()
+            if self.__solve():
+                print()
+                print("Solution:")
+                self.print_grid()
+            else:
+                print()
+                print("No solution for the current grid!")
 
     def print_grid(self):
-        """Prints the Sudoku Grid (9x9)"""
+        """Prints the 9x9 Sudoku Grid"""
+        print()
         for row in self.grid:
             for num in row:
                 print(num, end="  ")
             print()
 
-    def get_blocks(self):
-        """Converts 9x9 Sudoku grid into 9 Blocks of 3x3 and Returns it"""
-        blocks = [[[self.grid[i][j] for i in range(3 * x + 0, 3 * x + 3)
-                    for j in range(3 * y + 0, 3 * y + 3)]
-                   for y in range(3)]
-                  for x in range(3)]
-
-        return blocks
-
-    @staticmethod
-    def print_block(block):
-        """Prints the 3x3 Block"""
-        for i in range(9):
-            if i % 3 == 0:
-                print()
-            print(block[i], end=" ")
-
 
 if __name__ == "__main__":
     s = Sudoku()
-    print()
-    print("SUDOKU Grid :\n")
-    s.print_grid()
-    print()
 
-    blocks_ = s.get_blocks()
+    s.solve()  # Solving generated grid
 
-    for x in range(3):
-        for y in range(3):
-            print()
-            print(f"Block[{x}][{y}]:")
-            s.print_block(blocks_[x][y])
-            print()
+    s.grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+              [6, 0, 0, 1, 9, 5, 0, 0, 0],
+              [0, 9, 8, 0, 0, 0, 0, 6, 0],
+              [8, 0, 0, 0, 6, 0, 0, 0, 3],
+              [4, 0, 0, 8, 0, 3, 0, 0, 1],
+              [7, 0, 0, 0, 2, 0, 0, 0, 6],
+              [0, 6, 0, 0, 0, 0, 2, 8, 0],
+              [0, 0, 0, 4, 1, 9, 0, 0, 5],
+              [0, 0, 0, 0, 8, 0, 0, 0, 0]]
+
+    s.solve()  # Solving Custom grid
